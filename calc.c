849d5b0efbfef1f64bbf8e8403d1dc3593f44584
Ryan Miller <ryanmillera@gmail.com>
2015-06-22 23:14:19 -0500

    Added three more operations

--- calc.c
+++ calc.c
@@ -2,49 +2,71 @@
 #include <stdlib.h>
 #include <assert.h>
 
-int readArg(char *arg);
+typedef enum {ADD, SUBTRACT, MULTIPLY, DIVIDE} operators;
 
-struct Operation{
+int isOperator(char *arg);
+operators determineOperation(char operation);
+
+
+struct Expression{
 	int operandLeft;
 	int operandRight;
+	operators operator; 
 };
 
-struct Operation *Operation_create(int operandLeft, int operandRight)
+struct Expression *Expression_create(int operandLeft, int operandRight, operators operator)
 {
-	struct Operation *add = malloc(sizeof(struct Operation));
-	add->operandLeft = operandLeft;
-	add->operandRight = operandRight;
+	struct Expression *expression = malloc(sizeof(struct Expression));
+	expression->operandLeft = operandLeft;
+	expression->operandRight = operandRight;
+	expression->operator = operator;
 
-	return add;
+	return expression;
 }
 
-void Operation_destroy(struct Operation *add){
-	assert(add != NULL);
-	free(add);
+void Expression_destroy(struct Expression *expression){
+	assert(expression != NULL);
+	free(expression);
 }
 
-int Operation_add(struct Operation *add)
+int Expression_operate(struct Expression *expression)
 {
-	return add->operandLeft + add->operandRight;	
+	switch(expression->operator) {
+		case ADD:
+			return(expression->operandLeft + expression->operandRight);
+			break;
+		case SUBTRACT:
+			return(expression->operandLeft - expression->operandRight);
+			break;
+		case MULTIPLY:
+			return(expression->operandLeft * expression->operandRight);
+			break;
+		case DIVIDE:
+			return(expression->operandLeft / expression->operandRight);
+			break;
+		default:
+			return(0);
+	}	
 }
 
 int main(int argc, char *argv[])
 {
 	int i = 1;
-	int op = 0;
+	char operation = ' ';
+	int opFlag = 0;
 	int left = 0;
 	int right = 0;
 	int result = 0;
 
 	for(i = 1; i < argc; i++) {
-		op = readArg(argv[i]);
-		if(op == 1) {
-			i++;
-			right = atoi (argv[i]);
-			struct Operation *operation = Operation_create(left, right);
-			result = Operation_add(operation);
+		opFlag = isOperator(argv[i]);
+		if(opFlag == 1) {
+			operation = *argv[i++];
+			right = atoi(argv[i]);
+			struct Expression *expression = Expression_create(left, right, determineOperation(operation));
+			result = Expression_operate(expression);
 			printf("%d\n", result);
-			Operation_destroy(operation);
+			Expression_destroy(expression);
 		} else {
 			left = atoi (argv[i]);
 		}
@@ -52,11 +74,33 @@ int main(int argc, char *argv[])
 	return 0;
 }
 
-int readArg(char *arg)
+int isOperator(char *arg)
 {
-	if(*arg == 43) {
+	if(*arg == 43 || *arg == 45 || *arg == 120 || *arg == 47) {
 		return 1;
 	} else {
 		return 0;
 	}
+}
+
+operators determineOperation(char operation)
+{
+	switch(operation) {
+		case '+':
+			return(ADD);
+			break;
+		case '-':
+			return(SUBTRACT);
+			break;
+		//Cannot use '*' because shells reserve that character. There might be a workaround?
+		case 'x':
+			return(MULTIPLY);
+			printf("Made it here");
+			break;
+		case '/':
+			return(DIVIDE);
+			break;
+		default:
+			return(ADD);
+	}			
 }
